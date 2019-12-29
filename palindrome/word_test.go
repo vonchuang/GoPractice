d9f661612b1b4763d2a7d5ccf31821276faad787
vonchuang <chuang8673@gmail.com>
2019-12-30 00:29:23 +0800

    Add random test

--- palindrome/word_test.go
+++ palindrome/word_test.go
@@ -9,7 +9,9 @@ $ go test -v -run="French|Canal" 	(only test French and Canal)
 package word
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestPalindrome(t *testing.T) {
@@ -58,5 +60,47 @@ func TestCanalPalindrome(t *testing.T) {
 	input := "A man, a plan, a canal: Panama"
 	if !IsPalindrome(input) {
 		t.Error(`IsPalindrome("A man, a plan, a canal: Panama") = true`)
+	}
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // random len(max: 24)
+	runes := make([]rune, 25)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000)) // random rune(max: \u-0999)
+		runes[i] = r
+		runes[n-i-1] = r
+	}
+	return string(runes)
+}
+
+func randomNonPalindrome(rng *rand.Rand) string {
+	n := rand.Intn(25) + 2
+	runes := make([]rune, 27)
+	for i := 0; i < (n+1)/2; i++ {
+		tmp := 0x62 + rng.Intn(0x79-0x62)
+		runes[i] = rune(tmp)
+		runes[n-i-1] = rune(tmp + 1)
+	}
+	return string(runes)
+}
+
+func TestRandomPalindrome(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		seed := time.Now().UTC().UTC().UnixNano()
+		rng := rand.New(rand.NewSource(seed))
+		// test palindrome
+		s := randomPalindrome(rng)
+		if !IsPalindrome(s) {
+			t.Logf("Random seed: %d", seed)
+			t.Errorf("IsPalindrome(%q) = false", s)
+		}
+
+		// test non-palindrome
+		ss := randomNonPalindrome(rng)
+		//t.Logf(ss)
+		if IsPalindrome(ss) {
+			t.Errorf("IsPalindrome(\"%s\") = true", ss)
+		}
 	}
 }
